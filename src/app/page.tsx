32e3dabebdead1565e437879e63bfd02c569b5c6
avrhahm sason <avi6190i@gmail.com>
2026-02-10 10:18:09 +0200

    added some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="">
-            <main className=""></main>
+        <div className="size-full _center">
+            <main className="">
+                <h1></h1>
+            </main>
         </div>
     );
 }
--- src/lib/hooks/global.ts
+++ src/lib/hooks/global.ts
@@ -1,31 +1,24 @@
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export const useTranslationHelpers = () => {
     const { t } = useTranslation();
-    const getLengthError = (entity: string, length: number) => {
-        return t("length_error").replace("{entity}", entity).replace("{length}", length.toString());
-    };
-    const getSelectEntity = (entity: string) => {
-        return t("select_entity").replace("{entity}", entity);
-    };
-    const getServerError = (action: string) => {
-        return t("server_error").replace("{action}", action);
-    };
-    const getDeleteEntity = (entity: string) => {
-        return t("delete_entity").replace("{entity}", entity);
-    };
-    const getEditEntity = (entity: string) => {
-        return t("edit_entity").replace("{entity}", entity);
-    };
-    const getAddEntity = (entity: string) => {
-        return t("add_entity").replace("{entity}", entity);
-    };
-    const getNotFoundEntity = (entity: string) => {
-        return t("entity_not_found").replace("{entity}", entity);
-    };
+    const translationsFunctions = useMemo(
+        () => ({
+            getLengthError: (entity: string, length: number) => t("length_error").replace("{entity}", entity).replace("{length}", length.toString()),
+            getSelectEntity: (entity: string) => t("select_entity").replace("{entity}", entity),
+            getServerError: (action: string) => t("server_error").replace("{action}", action),
+            getDeleteEntity: (entity: string) => t("delete_entity").replace("{entity}", entity),
+            getEditEntity: (entity: string) => t("edit_entity").replace("{entity}", entity),
+            getAddEntity: (entity: string) => t("add_entity").replace("{entity}", entity),
+            getCreateEntity: (entity: string) => t("create_entity").replace("{entity}", entity),
+            getActionEntity: (entity: string) => t("action_entity").replace("{entity}", entity),
+            getNotFoundEntity: (entity: string) => t("entity_not_found").replace("{entity}", entity),
+        }),
+        [t]
+    );
 
-    return { getLengthError, getSelectEntity, getServerError, getDeleteEntity, getEditEntity, getAddEntity, getNotFoundEntity };
+    return translationsFunctions;
 };
 
 type UseFetchDataOptions = {
@@ -36,7 +29,6 @@ export const useFetchData = (options: UseFetchDataOptions = {}) => {
     const { initialLoading = false } = options;
     const [isLoading, setIsLoading] = useState(initialLoading);
     const pendingCountRef = useRef(0);
-
     const fetchData = async <T = any>(callback: () => Promise<T>) => {
         pendingCountRef.current += 1;
         setIsLoading(true);
